e2892c1b4ee9a67e8422b41d79acc18ed7dd4885
Camilla Alves <97700262+camillaalves12@users.noreply.github.com>
2022-05-09 00:02:21 -0300

    Add files via upload
    adicionando os arquivos

---
+++ index.js
@@ -0,0 +1,78 @@
+var flamengo = {
+  nome: "Flamengo",
+  vitorias: 2,
+  empates: 0,
+  derrotas: 0,
+  pontos: 0,
+};
+var cam = {
+  nome: "CAM",
+  vitorias: 0,
+  empates: 1,
+  derrotas: 1,
+  pontos: 0,
+};
+var palmeiras = {
+  nome: "Palmeiras",
+  vitorias: 0,
+  empates: 1,
+  derrotas: 1,
+  pontos: 0,
+};
+
+function calcularPontos(jogador) {
+  var pontos = jogador.vitorias * 3 + jogador.empates;
+  return pontos;
+}
+
+flamengo.pontos = calcularPontos(flamengo);
+cam.pontos = calcularPontos(cam);
+palmeiras.pontos = calcularPontos(palmeiras);
+
+var jogadores = [flamengo, cam, palmeiras];
+
+function exibirJogadoresTela(jogadores) {
+  var elemento = "";
+
+  for (var i = 0; i < jogadores.length; i++) {
+    elemento += "<tr><td>" + jogadores[i].nome + "</td>";
+    elemento += "<td>" + jogadores[i].vitorias + "</td>";
+    elemento += "<td>" + jogadores[i].empates + "</td>";
+    elemento += "<td>" + jogadores[i].derrotas + "</td>";
+    elemento += "<td>" + jogadores[i].pontos + "</td>";
+    elemento +=
+      "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>";
+    elemento +=
+      "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>";
+    elemento += "</tr>";
+  }
+
+  var tabelaJogadores = document.getElementById("tabelaJogadores");
+  tabelaJogadores.innerHTML = elemento;
+}
+
+exibirJogadoresTela(jogadores);
+
+function adicionarVitoria(i) {
+  var jogador = jogadores[i];
+  jogador.vitorias++;
+  jogador.pontos = calcularPontos(jogador);
+
+  exibirJogadoresTela(jogadores);
+}
+
+function adicionarEmpate(i) {
+  var jogador = jogadores[i];
+  jogador.empates++;
+  jogador.pontos = calcularPontos(jogador);
+
+  exibirJogadoresTela(jogadores);
+}
+
+function adicionarDerrota(i) {
+  var jogador = jogadores[i];
+  jogador.derrotas++;
+  exibirJogadoresTela(jogadores);
+}
